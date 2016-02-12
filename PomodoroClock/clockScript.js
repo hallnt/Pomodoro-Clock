40f4ba5c7bae692d5850afef30b63af9a0bbcbb3
Teleisha Hall <nickyb63@hotmail.com>
2016-02-11 23:58:35 -0500

    added some button functionality using JQuery and JavaScript

---
+++ PomodoroClock/clockScript.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function() {
+  
+  var sessionLength = 25;
+  var breakLength = 5;
+  
+  var seconds = 0;
+  var minutes = 25;
+  var counter;
+  
+  $('#timer').html(minutes + ":00");
+  
+  $('#minusBl').click(function() {
+      breakLength--;               
+      $('#bl').html(breakLength);
+  });
+
+  $('#plusBl').click(function () {
+      breakLength++;
+      $('#bl').html(breakLength);
+  });
+
+  $('#minusSl').click(function () {
+      sessionLength--;
+      $('#sl').html(sessionLength);
+  });
+
+  $('#plusSl').click(function () {
+      sessionLength++;
+      $('#sl').html(sessionLength);
+  });
+
+});
